59291e1f4746d039022c1ab63152bcb9d5e8515e
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2021-01-06 23:34:57 -0300

    initial commit

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#### Start http service
+
+#sudo service httpd stop
+sudo systemctl start httpd.service
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,6 @@
+
+# delete all content of folder html
+rm -rf /var/www/html
+
+#update the system
+sudo yum -y update
